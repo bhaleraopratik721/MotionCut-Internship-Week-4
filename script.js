ca025b8ca3639e10b689aed0a38aaea6760e3897
bhaleraopratik721 <148270477+bhaleraopratik721@users.noreply.github.com>
2024-02-06 19:17:05 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+const inputbox = document.getElementById("input-box");
+const listcontainer = document.getElementById("list-container");
+function addTask(){
+    if(inputBox.value === ''){
+        alert("You must write something!");
+    }
+    else{
+        let li=document.createElement("li");
+        li.innerHTML=inputBox.value;
+        listContainer.appendChild(li);
+    }
+}
